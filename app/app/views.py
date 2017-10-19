bbf08db8f13f617864bf4d5b9463e838fc8161bb
Katty <e.shchuplova@gmail.com>
2017-10-19 20:49:41 +0300

    docker preparation

--- app/app/views.py
+++ app/app/views.py
@@ -1,11 +1,13 @@
+from datetime import datetime, timedelta
+
 from flask import render_template, redirect, request, jsonify, url_for
-from app import app, models, db, login_manager, mail
-from .forms import LoginForm, RegisterForm, RegisterKeyForm, DeactivationKeyForm
-from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime, date, time, timedelta
-from flask_login import LoginManager, UserMixin, current_user, login_user, login_required, logout_user
+from flask_login import current_user, login_user, login_required, logout_user
+from flask_mail import Message
 from sqlalchemy import desc
-from flask_mail import Mail, Message
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import app, db, mail
+from app.app import models
 
 
 # авторизация пользователей
